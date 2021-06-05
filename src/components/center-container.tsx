39a48f80dced3c59f52cedfe43bd49b1c683970a
Shane <shane.richards121@gmail.com>
2021-06-05 04:19:04 -0400

    feat: add projects section

--- src/components/center-container.tsx
+++ src/components/center-container.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const CenterContainer: React.FC = ({ children}) => (
-  <div className="max-w-4xl mx-auto">
+  <div className="max-w-4xl mx-auto px-8">
     {children}
   </div>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,63 @@ import Layout from "../components/layout"
 import CenterContainer from "../components/center-container"
 import profilePicture from "../images/profile-pic.jpg"
 
+const projects = [
+  {
+    emoji: "💰",
+    title: "Crowd Coin",
+    description: "A lottery powered by Ethereum.",
+    link: "https://github.com/ShaneRich5/crowd-coin-web",
+  },
+  {
+    emoji: "🌤️",
+    title: "Meteo",
+    description: "A minimal Tailwind weather app.",
+    link: "https://meteo-forecast.netlify.app/",
+  },
+  {
+    emoji: "🖥️",
+    title: "Github IO",
+    description: "Source code for this Gatsby website.",
+    link: "https://github.com/ShaneRich5/shanerich5.github.io",
+  },
+  {
+    emoji: "🐷",
+    title: "Pig Latin",
+    description: "A fun spin on Google Translate.",
+    link: "https://github.com/ShaneRich5/piglatin-android",
+  },
+  {
+    emoji: "📸",
+    title: "Meme Me",
+    description: "A meme generating machine.",
+    link: "https://github.com/ShaneRich5/meme-me-v2",
+  },
+  {
+    emoji: "🖼️",
+    title: "Photoshop Battles",
+    description: "Photo competition from the best subreddit.",
+    link: "https://github.com/ShaneRich5/photoshop-battles",
+  },
+  {
+    emoji: "👂",
+    title: "Morse Code",
+    description: ".... . .-.. .-.. --- / .-- --- .-. .-.. -..",
+    link: "https://github.com/ShaneRich5/morse-code",
+  },
+  {
+    emoji: "🗺️",
+    title: "On The Map",
+    description: "A virtual hub for student globaly.",
+    link: "https://github.com/ShaneRich5/on-the-map",
+  },
+  {
+    emoji: "🎵",
+    title: "Pitch Perfect",
+    description: "Adjusts the sound of your voice.",
+    link: "https://github.com/ShaneRich5/pitch-perfect",
+  },
+]
+
 const IndexPage = () => {
   return (
     <Layout>
@@ -25,9 +82,23 @@ const IndexPage = () => {
       </section>
       <section className="my-14">
         <CenterContainer>
-          <h3 className="text-gray-800 font-mono font-bold text-4xl border-b-4 border-gray-800 mb-6 pb-2">
-            Projects.
+          <h3 className="text-gray-800 font-mono border-b-4 border-gray-800 mb-6 pb-2 flex justify-between">
+            <span className="font-bold text-4xl ">Favorite Projects.</span>
+            <a href="/projects" className="bg-gray-100 text-base p-2 rounded hover:bg-gray-200">View all</a>
           </h3>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-12 py-3">
+            {projects.map(project => (
+              <div className="col-span-1" key={project.title}>
+                <a href={project.link} target="_blank">
+                  <h4 className="text-2xl flex">
+                    <span className="mr-2">{project.emoji}</span>
+                    <span className="underline font-semibold text-gray-500">{project.title}</span>
+                  </h4>
+                </a>
+                <p className="text-xl text-gray-400 mt-2">{project.description}</p>
+              </div>
+            ))}
+          </div>
         </CenterContainer>
       </section>
     </Layout>
