44174fbb3e1b61590b12e5b8b655352151f200d1
Shane <shane.richards121@gmail.com>
2021-06-04 23:30:55 -0400

    feat: complete basic header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import React from "react"
+import logo from "../images/logo-icon.png"
 
 const IndexPage = () => {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-white">
       <title>Shane Richards</title>
-      <div className="min-h-screen flex justify-center items-center bg-gray-200">
-        <h1 className="text-7xl font-mono font-semibold">Hello World</h1>
-      </div>
+      <header className="max-w-3xl mx-auto my-8">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2">
+            <img src={logo} width={42}  alt="floppy disk logo" />
+            <h1 className="font-sans font-semibold text-2xl">Shane Richards</h1>
+          </div>
+          <nav className="space-x-6">
+            <a className="font-sans text-xl" href="/projects">Projects</a>
+            <a className="font-sans text-xl" href="/about">About Me</a>
+            <a className="font-sans text-xl" href="/about">Resume</a>
+          </nav>
+        </div>
+      </header>
     </main>
   )
 }
