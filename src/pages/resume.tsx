fb640e9655926969a9f552fecfb23b044e4bd0ab
Shane <shane.richards121@gmail.com>
2021-06-05 12:05:24 -0400

    feat: adding social links

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -5,9 +5,15 @@ import CenterContainer from "../components/center-container"
 const ResumePage = () => {
   return (
     <Layout>
-      <CenterContainer>
-        <h1>Resume Page</h1>
-      </CenterContainer>
+      <title>Resume | Shane Richards</title>
+      <section className="my-14">
+        <CenterContainer>
+          <h3 className="text-gray-800 font-mono my-6">
+            <span className="font-bold text-5xl">Shane Richards</span>
+          </h3>
+          <p className="text-2xl text-gray-500">Software Engineer in New York, NY</p>
+        </CenterContainer>
+      </section>
     </Layout>
   )
 }
