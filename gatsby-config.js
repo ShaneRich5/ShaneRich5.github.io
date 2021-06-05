8323a116aa6964a563a0343250a788adc412b303
Shane <shane.richards121@gmail.com>
2021-06-05 11:00:38 -0400

    feat: configure maniest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,18 @@ module.exports = {
   siteMetadata: {
     title: "Shane's Blog",
   },
-  plugins: ['gatsby-plugin-postcss'],
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        start_url: "/",
+        name: "Shane's Blog",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icon: "src/images/floppy-disk-icon.png"
+      }
+    }
+  ],
 };
