0a3c3b08b77d92f67705a39dcc4ed9869f5d1ab0
Shane <shane.richards121@gmail.com>
2021-06-04 22:30:18 -0400

    feat: create initial page, add tailwind styling

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const IndexPage = () => {
+  return (
+    <main className="min-h-screen">
+      <title>Shane Richards</title>
+      <div className="min-h-screen flex justify-center items-center bg-gray-200">
+        <h1 className="text-7xl font-mono font-semibold">Hello World</h1>
+      </div>
+    </main>
+  )
+}
+
+export default IndexPage
